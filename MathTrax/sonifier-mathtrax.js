c67a714d233978bb7caf2e75cbc9b5e97766c9c6
Shepazu <schepers@w3.org>
2013-09-23 11:12:39 -0400

    added animated cursor, key controls

--- MathTrax/sonifier-mathtrax.js
+++ MathTrax/sonifier-mathtrax.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function Sonifier() {
   this.svgroot = null;
   this.cursor = null;
@@ -6,6 +8,7 @@ function Sonifier() {
   this.context = null;
   this.oscillator = null;
   this.volume = null;
+  this.volumeLevel = 1;
   this.chartarea = null;
   this.dataLine = null;
   this.dataLinePoints = null;
@@ -13,8 +16,16 @@ function Sonifier() {
   this.maxx = 0;
   this.miny = 0;
   this.maxy = 0;
+  this.axisX = null;
+  this.axisY = null;
   this.valuePoint = null;
+  this.isMute = false;
   this.isPlaying = false;
+  this.timer = null;
+  this.cursorSpeed = 25;
+  this.cursorDirection = 1;
+  this.cursorColor = "white";
+  this.cursorIntersect = false;
 
   this.coords = null;
 
@@ -22,19 +33,23 @@ function Sonifier() {
   this.svgns = "http://www.w3.org/2000/svg";
 }
 
-Sonifier.prototype.init = function ( svgroot, chartarea, dataLine, width, height, color ) {	
+Sonifier.prototype.init = function ( svgroot, chartarea, dataLine, width, height, xAxis, yAxis, color ) {	
   this.svgroot = svgroot;
 	this.chartarea = chartarea;
 	this.dataLine = dataLine;
 	this.maxx = width;
 	this.maxy = height;
   this.coords = this.svgroot.createSVGPoint();
+  this.axisX = new Axis( xAxis[0], xAxis[1], 0, width);
+  this.axisY = new Axis( yAxis[0], yAxis[1], 0, height);
+  this.cursorColor = color;
 
+	// create cursor line and point
   this.cursor = document.createElementNS(this.svgns, 'path');
   this.cursor.setAttribute("id", "cursor");
   this.cursor.setAttribute("d", "M0,0 0," + this.maxy);
   this.cursor.setAttribute("fill", "none");
-  this.cursor.setAttribute("stroke", color);
+  this.cursor.setAttribute("stroke", this.cursorColor);
   this.cursor.setAttribute("stroke-width", "1");
   this.cursor.setAttribute("stroke-linecap", "round");
   this.cursor.setAttribute("stroke-dasharray", "5");
@@ -43,51 +58,129 @@ Sonifier.prototype.init = function ( svgroot, chartarea, dataLine, width, height
   this.cursorpoint = document.createElementNS(this.svgns, "circle");
   this.cursorpoint.setAttribute("id", "cursorpoint");
   this.cursorpoint.setAttribute("cx", 0 );
-  this.cursorpoint.setAttribute("cy", this.maxy );
+  this.cursorpoint.setAttribute("cy", this.miny );
   this.cursorpoint.setAttribute("r", 3 );
   this.cursorpoint.setAttribute("fill", "none");
-  this.cursorpoint.setAttribute("stroke", color);
+  this.cursorpoint.setAttribute("stroke", "none");
   this.chartarea.appendChild( this.cursorpoint );
 
-  this.svgroot.addEventListener('mousemove', bind(this, this.trackPointer), false );
+	this.setFrequency(0);
+
+  this.chartarea.addEventListener('mousemove', bind(this, this.trackPointer), false );
   document.documentElement.addEventListener('keydown', bind(this, this.trackKeys), false );
-  // document.documentElement.addEventListener('keyup', bind(this, this.trackKeys), false );
   // document.documentElement.addEventListener('click', this.toggleAudio, false );
 };
 
-Sonifier.prototype.toggleAudio = function (event) { 
-  //this.oscillator.stop();
-}   
 
 Sonifier.prototype.trackKeys = function (event) {
-  // if ( event.shiftKey ) {
-  // }
-
-  this.coords.y = 0;
-
-  var x = parseFloat( this.cursorpoint.getAttribute( "cx" ) );
+	var key = event.keyIdentifier.toLowerCase();
   
-  switch ( event.keyIdentifier) {
-    case "Down":
-    case "Right":
-      x += 1;
-      // console.log( event ) 
+  switch ( key ) {
+    case "down":
+    case "right":
+      this.stepCursor( 1 );
       break;
 
-    case "Up":
-    case "Left":
-      x -= 1;
-      // console.log( event ) 
+    case "up":
+    case "left":
+    this.stepCursor( -1 );
       break;
 
-    case "Enter":
-    case "Space":
+    case "enter":
       this.speak();
       break;
-  }
 
-  this.coords.x = x;
-  this.updateCursor();
+
+    case "p":
+    case "u+0050":
+      this.play();
+      break;
+
+    case "s":
+    case "u+0053":
+      this.resetPlay();
+      break;
+
+    case "[":
+    case "u+005b":
+      this.setPlayRate( 10 );
+      break;
+
+    case "]":
+    case "u+005d":
+      this.setPlayRate( -10 );
+      break;
+
+		case "m":
+		case "u+004d":
+			this.toggleVolume();
+			break;
+
+		case "o":
+		case "u+004f":
+			this.setDirection();
+			break;
+  }
+}   
+
+
+Sonifier.prototype.play = function ( xAxis, yAxis ) {
+	if ( this.timer ) {
+		this.stopPlay();
+	} else {
+		var t = this; 
+
+		this.timer = setInterval( function() { 
+			t.coords.x += t.cursorDirection;
+		  t.updateCursor();
+		}, t.cursorSpeed)
+	}
+}   
+
+Sonifier.prototype.stopPlay = function ( x, y ) { 
+	clearInterval( this.timer );
+	this.timer = null;
+}   
+
+Sonifier.prototype.resetPlay = function () { 
+	this.stopPlay( 1, 1 );
+	this.coords.x = 0;
+	this.coords.y = 0;
+	this.positionCursor( this.coords.x, this.coords.y, true );
+	this.setVolume( 0 );
+}   
+
+Sonifier.prototype.setPlayRate = function ( rateDelta ) { 
+	this.cursorSpeed += rateDelta;
+	if ( this.timer ) {
+		this.stopPlay();
+		this.play();
+	}
+}   
+
+Sonifier.prototype.stepCursor = function ( direction ) { 
+	var x = parseFloat( this.cursorpoint.getAttribute( "cx" ) );
+	this.coords.x = x + direction;
+	// this.coords.y = 0;
+	this.updateCursor();	
+}   
+
+
+Sonifier.prototype.setDirection = function () { 
+  if ( 1 == this.cursorDirection ) {
+		this.cursorDirection = -1;
+	} else {
+		this.cursorDirection = 1;
+	}
+	
+	if ( !this.timer ) {
+		this.play();
+	}
+}   
+
+
+Sonifier.prototype.setRange = function ( xAxis, yAxis ) { 
+  //this.oscillator.stop();
 }   
 
 Sonifier.prototype.trackPointer = function (event) { 
@@ -104,51 +197,82 @@ Sonifier.prototype.updateCursor = function () {
   // clip to range
   var x = Math.max( Math.min( this.maxx, this.coords.x ), this.minx );
 
-  var cursor_p1 = new Point2D(x, this.miny);
-  var cursor_p2 = new Point2D(x, this.maxy);
-
-  if (!this.dataLinePoints) {
-    this.dataLinePoints = [];
-    // console.log( this.dataLine.getAttribute("d") ) 
-    var dataLineArray = this.dataLine.getAttribute("d").split('L');
-    // console.log( dataLineArray ) 
-
-    for (var vp in dataLineArray ) {
-			if (typeof dataLineArray[vp] != 'function') { 
-	      var values = dataLineArray[vp].replace(/[A-Za-z]+/g, '').split(' ');
-	      this.dataLinePoints.push( 
-					new Point2D( 
-						parseFloat(values[0]), 
-						parseFloat(values[1]) 
-					) 
-				);
-			}
-    }
-  }
-  
-
-  // update cursor line
-  this.cursor.setAttribute('d', "M" + x + "," + this.miny + " " + x + "," + this.maxy);
-
-  // find intersection
-  var intersections = Intersection.intersectLinePolygon(
-    cursor_p1, cursor_p2, this.dataLinePoints
-  );
-
-  this.valuePoint = intersections.points[0];
-	if (!this.valuePoint) {
-		this.valuePoint = {x:0, y:0};
-		this.setVolume( 0 );
+	if ( ( this.maxx == x && 1 == this.cursorDirection ) 
+		|| ( 0 == x && -1 == this.cursorDirection ) ) {
+		this.stopPlay();
 	} else {
-		this.setVolume( 1 );
-		// console.log(intersections.points)
+	  var cursor_p1 = new Point2D(x, this.miny);
+	  var cursor_p2 = new Point2D(x, this.maxy);
+
+	  if (!this.dataLinePoints) {
+	    this.dataLinePoints = [];
+	    var dataLineArray = this.dataLine.getAttribute("d").split('L');
+
+	    for (var vp in dataLineArray ) {
+				if (typeof dataLineArray[vp] != 'function') { 
+		      var values = dataLineArray[vp].replace(/[A-Za-z]+/g, '').split(' ');
+		      this.dataLinePoints.push( 
+						new Point2D( 
+							parseFloat(values[0]), 
+							parseFloat(values[1]) 
+						) 
+					);
+				}
+	    }
+	  }
+
+	  // update cursor line
+	  this.cursor.setAttribute('d', "M" + x + "," + this.miny + " " + x + "," + this.maxy);
+
+	  // find intersection
+	  var intersections = Intersection.intersectLinePolygon(
+	    cursor_p1, cursor_p2, this.dataLinePoints
+	  );
+
+	  this.valuePoint = intersections.points[0];
+		if (this.valuePoint) {
+			if (!this.cursorIntersect) {
+				this.cursorIntersect = true;
+				this.setVolume( 1 );
+			  this.cursorpoint.setAttribute( "stroke", this.cursorColor );
+			}
+		} else {
+			this.valuePoint = {x:0, y:0};
+			if (this.cursorIntersect) {
+				this.cursorIntersect = false;				
+				this.setVolume( 0 );
+			  this.cursorpoint.setAttribute( "stroke", "none" );
+			}
+		}
+
+		// if (!this.valuePoint) {
+		// 	this.valuePoint = {x:0, y:0};
+		// 	if (this.cursorIntersect) {
+		// 		this.setVolume( 0 );
+		// 	  this.cursorpoint.setAttribute( "stroke", "none" );
+		// 		this.cursorIntersect = false;				
+		// 	}
+		// } else if (this.valuePoint && !this.cursorIntersect) {
+		// 	this.setVolume( 1 );
+		//   this.cursorpoint.setAttribute( "stroke", this.cursorColor );
+		// 	this.cursorIntersect = true;
+		// }
+
+		this.positionCursor( x, this.valuePoint.y, false );
+
+	  var frequency = 500 - this.valuePoint.y;
+	  this.setFrequency ( frequency );		
 	}
+}   
 
+Sonifier.prototype.positionCursor = function ( x, y, setLine ) { 
   this.cursorpoint.setAttribute( "cx", x );
-  this.cursorpoint.setAttribute( "cy", this.valuePoint.y );
+  this.cursorpoint.setAttribute( "cy", y );
 
-  var frequency = 500 - this.valuePoint.y;
-  this.setFrequency ( frequency )
+	if ( setLine ) {
+	  // update cursor line
+	  this.cursor.setAttribute('d', "M" + x + "," + this.miny + " " + x + "," + this.maxy);
+	}
 }
 
 
@@ -162,8 +286,8 @@ Sonifier.prototype.setFrequency = function ( frequency ) {
     this.oscillator.start(0);
 
     this.volume = this.context.createGainNode(); 
-    this.volume.connect(this.context.destination);
     this.oscillator.connect(this.volume);
+    this.volume.connect(this.context.destination);
     this.volume.gain.value = 0;
   }
 
@@ -178,13 +302,24 @@ Sonifier.prototype.setVolume = function ( gain ) {
 }
 
 
+Sonifier.prototype.toggleVolume = function () { 
+	if ( this.isMute ) {
+		this.isMute = false;
+    this.volume.connect(this.context.destination);
+	} else {
+		this.isMute = true;
+    this.volume.disconnect(this.context.destination);
+	}
+}
+
+
 Sonifier.prototype.speak = function () { 
   if ( "undefined" != typeof speechSynthesis ) {
 	  if ( speechSynthesis.speaking ) {
 	    speechSynthesis.cancel();
 	  }
-	
-		msg = Math.round( this.valuePoint.y * 10 ) / 10;
+		var msg = "x = " + this.axisX.scale( this.valuePoint.x );
+		msg += ", y = " + this.axisY.scale( this.valuePoint.y );
 
     var u = new SpeechSynthesisUtterance();
     u.text = msg;
@@ -212,6 +347,23 @@ function bind (scope, fn) {
 		return fn.apply( scope, arguments );
 	}
 }
+
+function Axis(min, max, chartMin, chartMax) {
+	if ( arguments.length > 0 ) {
+		this.min = min;
+		this.max = max;
+		this.chartMin = chartMin;
+		this.chartMax = chartMax;
+	}
+}
+
+Axis.prototype.scale = function( val ) {
+	var newVal = (val / ((this.chartMax - this.chartMin) / (this.max - this.min))) + this.min;
+	newVal = Math.round( newVal * 10 ) / 10;
+	return newVal;
+};
+
+
 
 /******
 * 
